ea5163d7a9afbd7ee3ae885dd55adcddcc611309
AcerFeng <lanfeng@lanfengdeMac-mini.local>
2017-01-13 10:47:48 +0800

    init project

---
+++ BasicProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicProject
+//
+//  Created by lanfeng on 17/1/13.
+//  Copyright © 2017年 lanfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
